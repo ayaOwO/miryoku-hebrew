9498fe9834e9726428cdf012414798e087954293
Itay Kraise <tatytoy1@gmail.com>
2021-10-25 23:24:36 +0300

    split to files

---
+++ customKeyCodes.h
@@ -0,0 +1,56 @@
+// custom keycodes used for changing language
+// alt + shift, changes base layer 
+enum custom_keycodes {
+    changeToDvorak = SAFE_RANGE,
+    changeToHebrew,
+    changeToQwerty
+};
+
+// custom keycodes
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    //set_keylog(keycode, record);
+    switch (keycode) {
+    case changeToDvorak:
+    {
+      // change language
+      if (layer_state_is(HEB))
+      {
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+      }
+        layer_move(BASE);
+        return false;
+
+      break;
+    }
+    case changeToHebrew:
+    {
+      // change language
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        layer_move(HEB);
+        return false;
+      break;
+    }
+    case changeToQwerty:
+    {
+      // change language
+      if (layer_state_is(HEB)){
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+      }
+      layer_move(GAME);
+      return false;
+      break;
+    }
+  }
+  }
+  return true;
+}
