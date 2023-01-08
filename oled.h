6a37280409af87c1a1b3e0d2bcadcd80132f429e
ayayawo <tatytoy1@gmail.com>
2023-01-08 23:41:40 +0200

    Added direction

--- oled.h
+++ oled.h
@@ -1,5 +1,10 @@
 // enables oled support
 #ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;
+}
+
 bool oled_task_user(void) {
     switch (get_highest_layer(layer_state)) {
         case U_NAV:
